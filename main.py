40520c7fde7fcfcd07207c18a594e173d8f5cc5b
ABHINAV JAIN <jain.abhinav85@gmail.com>
2021-07-23 02:40:25 +0100

    Policy Improvement

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from environments.frozen_lake_environment import FrozenLake
 from rl_algorithms.non_tabular_model_free import LinearWrapper, linear_sarsa, linear_q_learning
 from rl_algorithms.tabular_model_based import policy_iteration, value_iteration
 from rl_algorithms.tabular_model_free import sarsa, q_learning
-
+from rl_algorithms.tabular_model_based import policy_evaluation,policy_improvement  #test, remove when done
 
 def main():
     seed = 0
@@ -14,18 +14,43 @@ def main():
             ['#', '.', '.', '$']]
 
     env = FrozenLake(lake, slip=0.1, max_steps=16, seed=seed)
-    play(env)
+    # play(env)
 
-    # print('# Model-based algorithms')
+    # ------Test Pol Eval---------#
+    # print("EVALUATE INITIAL POLICY")
     # gamma = 0.9
     # theta = 0.001
     # max_iterations = 100
-    #
-    # print('')
-    #
-    # print('## Policy iteration')
-    # policy, value = policy_iteration(env, gamma, theta, max_iterations)
-    # env.render(policy, value)
+    # polTest = [3,3,2,1,2,2,2,2,3,3,2,2,2,3,3,3,3]
+    # value = policy_evaluation(env,polTest,gamma, theta, max_iterations)
+    # env.render(polTest,value)
+    #-----------------------------#
+
+    # ------Test Pol Improvement---------#
+    # print("\nIMPROVE POLICY")
+    # gamma = 0.9
+    # theta = 0.001
+    # max_iterations = 100
+    # polTest = [3,3,2,1,2,2,2,2,3,3,2,2,2,3,3,3,3]
+    # new_pol = policy_improvement(env,polTest,value,gamma)
+    # print("new policy:", new_pol)
+    # new_value = policy_evaluation(env,new_pol[0],gamma, theta, max_iterations)
+    # env.render(new_pol[0],new_value)
+    #-----------------------------#
+
+
+    print('# Model-based algorithms')
+    gamma = 0.9
+    theta = 0.001
+    max_iterations = 100
+
+    print('')
+
+    print('## Policy iteration')
+    # polTest = [3, 3, 2, 1, 2, 2, 2, 2, 3, 3, 2, 2, 2, 3, 3, 3, 3]
+    # policy, value = policy_iteration(env, gamma, theta, max_iterations,polTest)
+    policy, value = policy_iteration(env, gamma, theta, max_iterations)
+    env.render(policy, value)
     #
     # print('')
     #
--- rl_algorithms/tabular_model_based.py
+++ rl_algorithms/tabular_model_based.py
@@ -2,19 +2,42 @@ import numpy as np
 
 
 def policy_evaluation(env, policy, gamma, theta, max_iterations):
+
     value = np.zeros(env.n_states, dtype=np.float)
+    delta = abs(theta)+1  # Force the loop entry
+    i = 0
+    while (delta > theta and i < max_iterations):
+        delta = 0
+        for state in range(env.n_states):
+            value_old = value[state]
+            value_tmp = 0
+            for ns in range(env.n_states):
+                value_tmp += env.p(ns, state, action=policy[state]) * (
+                            (env.r(ns, state, action=policy[state])) + (gamma * value[ns]))
 
-    # TODO:
-
+            value[state] = value_tmp
+            delta = max(delta, np.abs(value_old - value[state]))
+        i += 1
     return value
 
+def policy_improvement(env, policy, value, gamma):
+    improved_policy = np.zeros(env.n_states, dtype=int)
+    policy_stable = True
+    for state in range(env.n_states):
+        old_action = policy[state]
+        new_actions = []
+        new_action_values = []
+        for action in range(env.n_actions):
+            for ns in range(env.n_states):
+                new_actions.append(action)
+                new_action_values.append(env.p(ns, state, action=action) * (
+                            (env.r(ns, state, action=action)) + (gamma * value[ns])))
+        best_action = new_actions[new_action_values.index(max(new_action_values))]
+        improved_policy[state] = best_action
+        if old_action != best_action: # and state != env.absorbing_state_idx:
+            policy_stable = False
 
-def policy_improvement(env, value, gamma):
-    policy = np.zeros(env.n_states, dtype=int)
-
-    # TODO:
-
-    return policy
+    return improved_policy, policy_stable
 
 
 def policy_iteration(env, gamma, theta, max_iterations, policy=None):
@@ -23,8 +46,11 @@ def policy_iteration(env, gamma, theta, max_iterations, policy=None):
     else:
         policy = np.array(policy, dtype=int)
 
-    # TODO:
-    value = None
+    while True:
+        value = policy_evaluation(env,policy,gamma,theta,max_iterations)
+        policy,policy_stable = policy_improvement(env,policy,value,gamma)
+        if policy_stable:
+            break
 
     return policy, value
 
